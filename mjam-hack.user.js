8b7a17dd7e9126464180aa3fefcce41801f4e62f
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2017-06-20 11:15:28 +0200

    Just a test version upgrade

--- mjam-hack.user.js
+++ mjam-hack.user.js
@@ -6,7 +6,7 @@
 // @include     http://mjam.net/*
 // @include     https://www.mjam.net/*
 // @include     https://mjam.net/*
-// @version     5
+// @version     6
 // @grant       GM_addStyle
 // @require http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js
 // @downloadURL https://github.com/Bronkoknorb/mjam-hack/raw/master/mjam-hack.user.js
@@ -14,6 +14,7 @@
 
 // we install a timer task to repeatedly unhide unavailable options from the menu
 var timer = setInterval(myTimer, 1000);
+
 function myTimer() {
     // unhide and display italic instead
     $("#menu_category_list li.unavailable").css('display', 'block').css('font-style', 'italic');
